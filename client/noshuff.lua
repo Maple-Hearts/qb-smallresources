3e344a8f53951c3547b68d10e481cacc267c4ad0
ItzDabbzz <itzdabbzz@gmail.com>
2023-06-18 08:53:43 -0700

    Fixed Seat Shuffle

--- client/noshuff.lua
+++ client/noshuff.lua
@@ -1,35 +1,6 @@
-local disableShuffle = true
-
-RegisterNetEvent('QBCore:Client:EnteredVehicle', function(data)
-    local sleep
-    local ped = PlayerPedId()
-    while IsPedInAnyVehicle(ped, false) do
-        sleep = 100
-        if IsPedInAnyVehicle(ped, false) and disableShuffle then
-            if GetPedInVehicleSeat(data.vehicle, 0) == ped then
-                if GetIsTaskActive(ped, 165) then
-                    sleep = 0
-                    SetPedIntoVehicle(ped, data.vehicle, 0)
-                    SetPedConfigFlag(ped, 184, true)
-                end
-            end
-        end
-        Wait(sleep)
+lib.onCache('vehicle', function(vehicle)
+    if vehicle and GetPedInVehicleSeat(vehicle, cache.seat) == cache.ped then
+        SetPedIntoVehicle(cache.ped, vehicle, cache.seat)
+        SetPedConfigFlag(cache.ped, 184, true)
     end
 end)
-
-RegisterNetEvent('SeatShuffle', function()
-    local ped = PlayerPedId()
-    if IsPedInAnyVehicle(ped, false) then
-        disableShuffle = false
-        SetPedConfigFlag(ped, 184, false)
-        Wait(3000)
-        disableShuffle = true
-    else
-        CancelEvent()
-    end
-end)
-
-RegisterCommand("shuff", function()
-    TriggerEvent("SeatShuffle")
-end, false)
